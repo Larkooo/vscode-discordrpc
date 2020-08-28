6c14420fef24f621234a78a7e6cdce218b45f901
Larko <59736843+Larkooo@users.noreply.github.com>
2020-08-27 20:15:32 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -43,29 +43,14 @@ function activate(context) {
 			instance: false,
 		  });
 	}
-	
+
 	rpc.on('ready', () => {
 		setActivity();
 		// activity can only be set every 15 seconds
 	});
 	rpc.login({ clientId }).catch(console.error);
 
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "discord-rich-presence" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('discord-rich-presence.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage(`${vscode.env.appName}`);
-	});
-
-	context.subscriptions.push(disposable);
+	
 }
 exports.activate = activate;
 
